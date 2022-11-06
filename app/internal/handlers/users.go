0423d333a26b2d918550762b46f6803bfc945372
fdmshd <kirh09764@gmail.com>
2022-11-06 20:59:29 +0700

    fix error messages in handler

--- app/internal/handlers/users.go
+++ app/internal/handlers/users.go
@@ -26,19 +26,19 @@ func (h *UserHandler) SetKey(key string) {
 func (h *UserHandler) Signup(c echo.Context) (err error) {
 	u := &models.User{}
 	if err = c.Bind(u); err != nil {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err}
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 	if err = h.Validator.Validate(u); err != nil {
 		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 	pw, err := utils.HashPassword(u.Password)
 	if err != nil {
-		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err}
+		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err.Error()}
 	}
 	u.Password = pw
 	id, err := h.UserModel.Insert(*u)
 	if err != nil {
-		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err}
+		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err.Error()}
 	}
 
 	return c.JSON(http.StatusCreated, fmt.Sprintf("id:%d", id))
@@ -47,12 +47,12 @@ func (h *UserHandler) Signup(c echo.Context) (err error) {
 func (h *UserHandler) Login(c echo.Context) (err error) {
 	u := new(models.User)
 	if err = c.Bind(u); err != nil {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err}
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 
 	user, err := h.UserModel.GetByName(u.Username)
 	if err != nil {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err}
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 
 	if !utils.CheckPasswordHash(u.Password, user.Password) {
@@ -67,7 +67,7 @@ func (h *UserHandler) Login(c echo.Context) (err error) {
 
 	user.Token, err = token.SignedString([]byte(h.key))
 	if err != nil {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err}
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 	user.Password = ""
 	return c.JSON(http.StatusOK, user)
@@ -78,11 +78,11 @@ func (h *UserHandler) Get(c echo.Context) (err error) {
 	id := c.Param("id")
 	idConv, err := strconv.Atoi(id)
 	if err != nil {
-		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err}
+		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err.Error()}
 	}
 	user, err := h.UserModel.Get(idConv)
 	if err != nil {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err}
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 	if currID != id {
 		user.Email = ""
@@ -97,7 +97,7 @@ func (h *UserHandler) GetAll(c echo.Context) (err error) {
 	}
 	users, err := h.UserModel.List()
 	if err != nil {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err}
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 	return c.JSON(http.StatusOK, users)
 }
@@ -110,14 +110,14 @@ func (h *UserHandler) Update(c echo.Context) (err error) {
 	}
 	u := new(models.User)
 	if err = c.Bind(u); err != nil {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err}
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 
 	if err = h.Validator.ValidateExcept(u, "Password"); err != nil {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err}
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 	if u.Id, err = strconv.Atoi(id); err != nil {
-		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err}
+		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err.Error()}
 	}
 	err = h.UserModel.Update(*u)
 	if err != nil {
@@ -134,11 +134,11 @@ func (h *UserHandler) Delete(c echo.Context) (err error) {
 	}
 	id, err := strconv.Atoi(idFromParam)
 	if err != nil {
-		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err}
+		return &echo.HTTPError{Code: http.StatusInternalServerError, Message: err.Error()}
 	}
 	err = h.Producer.Delete(id)
 	if err != nil {
-		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err}
+		return &echo.HTTPError{Code: http.StatusBadRequest, Message: err.Error()}
 	}
 	return c.JSON(http.StatusOK, id)
 }
